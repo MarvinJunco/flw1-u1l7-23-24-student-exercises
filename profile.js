803c0f0388a00b1d4108326c5711e6fff3b877b8
MarvinJunco <139893846+MarvinJunco@users.noreply.github.com>
2023-11-29 22:41:34 +0000

    finished

--- profile.js
+++ profile.js
@@ -7,10 +7,13 @@ let main = document.querySelector(".main");
 button.addEventListener("click", function() {
 
     // 1. Updated the innerHTML of profileName to your name or a made up one.
+profileName.innerHTML = "Marvin";
 
     // 2. Updated the src of profileImage a picture of you or one that makes sense.
+  profileImage.src = "https://play-lh.googleusercontent.com/k2hk4F0LgI-bqE2N2RYVe1k4wYavSWOpBCsX2G_GyvRZPdKl82V4gIOOk16fPJy8FQ=w600-h300-pc0xffffff-pd";
 
     // 3. Updated the src of main to a photo that's be your first post.
 
-
+  main.src = "https://i.ytimg.com/vi/TKfS5zVfGBc/maxresdefault.jpg";
+  
 });
--- shapes.js
+++ shapes.js
@@ -9,8 +9,11 @@ circle.addEventListener("click", function() {
     // - Set the background color to purple.
     // - Set the width to 150px.
     // - Set the height to 150px.
-   
 
+  circle.style.backgroundColor = "purple";
+  circle.style.width = "150px";
+  circle.style.height = "150px";
+  
 });
 
 square.addEventListener("click", function() {
@@ -20,7 +23,9 @@ square.addEventListener("click", function() {
     // - Set the width to 150px.
     // - Set the height to 150px.
 
-
+  square.style.backgroundColor = "yellow";
+  square.style.width = "150px";
+  square.style.height = "150px";
 
 });
 
@@ -30,8 +35,10 @@ rectangle.addEventListener("click", function() {
     // - Set the background color to orange.
     // - Set the width to 250px.
     // - Set the height to 150px.
-    
 
+  rectangle.style.backgroundColor = "orange";
+  rectangle.style.width = "250px";
+  rectangle.style.height = "150px";
 
 });
 
